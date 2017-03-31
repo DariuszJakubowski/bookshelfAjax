94f4808093610f51c13c54fafdead51e62d900e9
DariuszJakubowski <wielki.dekonstruktor@gmail.com>
2017-03-31 14:02:22 +0200

    created index.php with form,bootstrap style, front-end validation

--- public/index.php
+++ public/index.php
@@ -1,1 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Bookshelf - ajax</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="./js/app.js"></script>
+        <script src="./js/formValidation.js"></script>
+
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-static-top" >
+            <div class="container-fluid">
+                <div class=" navbar-header">
+                    <div class="navbar-brand">My books</div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container text-center" id="container">
+
+            <div class="row">
+
+                <div class="col-sm-4 col-sm-offset-2 ">
+                    <form method="post" class="form">
+                        <fieldset>
+                            <div class="form-group">
+                                <input id="isbn" name="isbn" class="form-control"  placeholder="isbn">
+                            </div>
+                            <div class="form-group">
+                                <input id="author" name="author" class="form-control"  placeholder="author" required>
+                            </div>
+                            <div class="form-group">
+                                <input id="title" name="title" class="form-control"  placeholder="title" required>
+                            </div>
+                            <div class="form-group">
+                                <textarea id="description" name="description" class="form-control"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" value="add book" class="btn btn-success btn-block">
+                                <input type="reset" value="clear all" class="btn btn-danger btn-block">
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
+                <div class="col-sm-4">
+                    tu będą wypisane książki
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+
+    </body>
+</html>
---
+++ public/js/formValidation.js
@@ -0,0 +1,46 @@
+
+$(function(){
+
+$('.btn-success').click(function(){
+    validateForm();   
+});
+
+function validateForm(){
+
+    var isbnReg =  /^((\d){9}|(\d){13})$/;
+  
+    var isbn = $('#isbn').val();
+    var author = $('#author').val();
+    var title = $('#title').val();
+    var description = $('#description').val();
+
+    var inputVal = [isbn, author, title, description];
+
+     $('.error').hide();
+
+        if(isbn == ""){
+            $('#isbn').after('<span class="error"> Wpisz proszę ISBN</span>');
+        } 
+        else if(!isbnReg.test(isbn)){
+            $('#isbn').after('<span class="error"> ISBN musi mieć 9 lub 13 cyfr</span>');
+        }
+
+        if(author == ""){
+            $('#author').after('<span class="error"> Wpisz proszę autora</span>');
+        } else if(author.length > 255){
+            $('#author').after('<span class="error">Max 255 znaków</span>');
+        }
+
+        if(title == ""){
+            $('#title').after('<span class="error"> Wpisz proszę tytuł</span>');
+        }  else if(title.length > 255){
+            $('#title').after('<span class="error">Max 255 znaków</span>');
+        }
+
+        if(description.length > 255){
+            $('#description').after('<span class="error"> Opis nie dłuższy niż 255 znaków</span>');
+        } 
+ 
+}   
+
+});
