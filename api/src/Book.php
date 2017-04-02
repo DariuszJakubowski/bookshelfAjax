db862d912230b3c13eef91e8fb9579bdcf004aff
DariuszJakubowski <wielki.dekonstruktor@gmail.com>
2017-04-02 23:55:17 +0200

    added features CRUD in api/src/Book.php (model)

--- api/src/Book.php
+++ api/src/Book.php
@@ -34,4 +34,58 @@ class Book
 
         return $booksFromDB;
     }
+
+        public function create($conn, int $isbn, string $author, string $title, string $description)
+    {
+        $stmt = $conn->prepare('INSERT INTO book (isbn, author, title, description) 
+            VALUES(?, ?, ?, ?)');
+
+        $stmt->bind_param('isss',
+            $this->validISBN($isbn),
+            $this->sanitizeString($author),
+            $this->sanitizeString($title),
+            $this->sanitizeString($description));
+        $stmt->execute();
+
+        $createIsDone = true; //!$stmt->errno;
+        $stmt->close();
+
+        return $createIsDone;
+    }
+
+    public function update($conn, int $isbn, string $author, string $title, string $description)
+    {
+        $stmt = $conn->prepare("UPDATE book SET isbn=?, author=?, title=?, description=? WHERE id=$this->id");
+        $stmt->bind_param('isss',
+            $this->validISBN($isbn),
+            $this->sanitizeString($author),
+            $this->sanitizeString($title),
+            $this->sanitizeString($description));
+        $stmt->execute();
+
+        $updateDone = !$stmt->errno;
+        $conn->close();
+        return $updateDone;
+    }
+
+    public function deleteFromDB($conn, int $id)
+    {
+
+        return ($conn->query("DELETE FROM book WHERE id = $id") === TRUE) ? TRUE
+                : FALSE;
+    }
+
+    private function validIsbn($isbn)
+    {
+
+        if (!preg_match('/^(\d){13}|(\d){9}$/', $isbn)) {
+            $isbn = FALSE;
+        }
+        return $isbn;
+    }
+
+    private function sanitizeString($str)
+    {
+        return trim(htmlspecialchars($str, ENT_QUOTES, 'UTF-8'));
+    }
 }
